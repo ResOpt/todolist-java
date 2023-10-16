db5ae3afa6423c1c7595e6898b17f79a21c4360a
Rahman Hakim <rahmanhakim2435@gmail.com>
2023-10-16 17:39:19 +0700

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+public class Main {
+    public static void main(String args[]) {
+
+        JFrame frame = new JFrame("Todo List");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+}
