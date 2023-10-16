015c591e09065de991f8f7de9da5a95869f412b6
ARYA ERSI PUTRA <138345958+ZGE26@users.noreply.github.com>
2023-10-16 20:22:54 +0700

    add one layout

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,19 @@ public class Main {
     public static void main(String args[]) {
 
         JFrame frame = new JFrame("Todotodo");
+
+        JButton button = new JButton("Pencet");
+
+        button.setBounds(50,100,100,100);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.add(button);
+
+        frame.add(button);
+
+        frame.add(panel);
+        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setVisible(true);
