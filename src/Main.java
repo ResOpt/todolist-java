3e821de37c3ebe00192835b83ba6d247685a957e
ARYA ERSI PUTRA <138345958+ZGE26@users.noreply.github.com>
2023-10-16 18:03:15 +0700

    ganti frame jadi todotodo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.awt.*;
 public class Main {
     public static void main(String args[]) {
 
-        JFrame frame = new JFrame("Todo List");
+        JFrame frame = new JFrame("Todotodo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setVisible(true);
